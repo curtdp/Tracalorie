6bea4410f435a309627b03f44d8ed70d09ac9594
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-04-11 23:26:34 +0300

    Теперь нужно, чтобы форма обрабатывала только событие клик, а не сабмит формы (клавиша enter) для этого задисейблим enter
    .which нужен для старых браузеров
    Если нажем enter теперь на форме, ни чего не произойдет
    Нужно вручную кликать мышью на кнопку update

--- app.js
+++ app.js
@@ -179,6 +179,14 @@ const App = (function(ItemCtrl, UICtrl) {
     // Add item event
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
 
+    // Disable submit on enter
+    document.addEventListener('keypress', function (e) {
+      if (e.keyCode === 13 || e.which === 13) {
+        e.preventDefault();
+        return false;
+      }
+    });
+
     // Edit icon click event
     document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
 
