d106684b89f4a5d9e39d989ae6c6d7bb26c0a6e2
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-04-11 22:15:11 +0300

    Обновляем сумму калорий из ItemCtrl.getTotalCalories получаем totalCalories
    Объявляем метод ItemCtrl.getTotalCalories
    В этом методе мы должны перебрать элементы
    и посчитать сумму калорий
    после чего возвращаем сумму калорий
    
    Показываем сумму калорий в UI
    Создаем метод UICtrl.showTotalCalories
    и создаем UISelector.totalCalories

--- app.js
+++ app.js
@@ -45,6 +45,19 @@ const ItemCtrl = (function() {
 
       return newItem;
     },
+    getTotalCalories: function () {
+      let total = 0;
+
+      // Loop through items and add cals
+      data.items.forEach(function (item) {
+        total += item.calories;
+      });
+      // Set total cal in data structure
+      data.totalCalories = total;
+
+      // Return total
+      return data.totalCalories;
+    },
     logData: function() {
       return data;
     }
@@ -59,6 +72,7 @@ const UICtrl = (function() {
     addBtn: '.add-btn',
     itemNameInput: '#item-name',
     itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories',
   }
 
   // Public methods
@@ -109,6 +123,9 @@ const UICtrl = (function() {
     hideList: function () {
       document.querySelector(UISelectors.itemList).style.display = 'none';
     },
+    showTotalCalories: function (totalCalories) {
+      document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+    },
     getSelectors: function () {
       return UISelectors;
     }
@@ -138,6 +155,11 @@ const App = (function(ItemCtrl, UICtrl) {
 
       // Add item to UI list
       UICtrl.addListItem(newItem);
+
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
 
       // Clear fields
       UICtrl.clearInput();
