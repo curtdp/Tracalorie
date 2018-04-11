911d641641f9cdba842d4dbe8c4c7ad6ba184483
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-04-11 23:17:42 +0300

    После этого мы хотим попасть в состояние редактирования когда появились кнопки update, delete и back
    Для этого вызовем метод UICtrl.showEditState
    
    и определим его
    
    Это противоположное от clearEditState
    Проверяем

--- app.js
+++ app.js
@@ -142,6 +142,7 @@ const UICtrl = (function() {
     addItemToForm: function () {
       document.querySelector(UISelectors.itemNameInput).value = ItemCtrl.getCurrentItem().name;
       document.querySelector(UISelectors.itemCaloriesInput).value = ItemCtrl.getCurrentItem().calories;
+      UICtrl.showEditState();
     },
     hideList: function () {
       document.querySelector(UISelectors.itemList).style.display = 'none';
@@ -155,6 +156,12 @@ const UICtrl = (function() {
       document.querySelector(UISelectors.deleteBtn).style.display = 'none';
       document.querySelector(UISelectors.backBtn).style.display = 'none';
       document.querySelector(UISelectors.addBtn).style.display = 'inline';
+    },
+    showEditState: function () {
+      document.querySelector(UISelectors.updateBtn).style.display = 'inline';
+      document.querySelector(UISelectors.deleteBtn).style.display = 'inline';
+      document.querySelector(UISelectors.backBtn).style.display = 'inline';
+      document.querySelector(UISelectors.addBtn).style.display = 'none';
     },
     getSelectors: function () {
       return UISelectors;
