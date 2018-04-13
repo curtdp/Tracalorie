62fc6872166d54935045756a3e2b15655508de22
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-04-13 21:24:38 +0300

    Делаем получение данных их локального хранилища И вызываем его для получения данных в ItemCtrl

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ const StorageCtrl = (function() {
         localStorage.setItem('items', JSON.stringify(items));
       }
     },
+    getItemsFromStorage: function () {
+      let items;
+      if (localStorage.getItem('items') === null) {
+        items = [];
+      } else {
+        items = JSON.parse(localStorage.getItem('items'));
+      }
+      return items;
+    },
   }
 })();
 
@@ -36,11 +45,12 @@ const ItemCtrl = (function() {
 
   // Data Structure / State
   const data = {
-    items: [
-        // { id: 0, name: 'Steak Dinner', calories: 1200},
-        // { id: 1, name: 'Cookie', calories: 400},
-        // { id: 2, name: 'Eggs', calories: 300}
-    ],
+    // items: [
+    //     // { id: 0, name: 'Steak Dinner', calories: 1200},
+    //     // { id: 1, name: 'Cookie', calories: 400},
+    //     // { id: 2, name: 'Eggs', calories: 300}
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0
   }
